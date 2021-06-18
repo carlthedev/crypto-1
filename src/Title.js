45e8d8ee2f979599c6329081c316ffc2614d674b
carl <carl.cross@yahoo.co.uk>
2021-06-18 04:27:04 +0100

    1st draft title component

---
+++ src/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './crypto-logo.png';
+
+function Title() {
+  return (
+    <div className="container">
+      <img src={logo} alt="Logo"/>
+      <h1>Crypto Squires</h1>
+    </div>
+  )
+}
+
+export default Title
